43155ad86a67310d066c2a0a231eb613c1042406
Viníciusfsa <viniciussantos123fsa@gmail.com>
2023-02-13 21:48:57 -0300

    creation of supplier route

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import clientRoute from "./routes/client.route.js";
+import supplierRoute from "./routes/supplier.route.js";
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 app.use("/client", clientRoute);
-// app.use("/supplier");
+app.use("/supplier", supplierRoute);
 // app.use("/product");
 // app.use("/sale");
 
